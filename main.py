79b1281a20289719fc2ec43d973fee37cd5a0bca
Li Zhang <lizhang@Lis-MacBook-Pro.local>
2020-03-14 13:33:46 -0500

    encapsulated function inside CharacterMapping class

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 import sys
 
+class CharacterMapping:
+    def checkMapping(self, s1, s2):
+        len1 = len(s1)
+        len2 = len(s2)
+        if len1 != len2:
+            return False
+
+        len1Unique = len(set(s1))
+        if len1Unique < len1:
+            return False
+
+        return True
+
 def main():
     if len(sys.argv) != 3:
         print('Invalid input: argument missing')
         return
     s1 = sys.argv[1]
     s2 = sys.argv[2]
-    len1 = len(s1)
-    len2 = len(s2)
-    if len1 != len2:
+    mapping = CharacterMapping()
+    if mapping.checkMapping(s1, s2):
+        print('true')
+    else:
         print('false')
-        return
-    len1Unique = len(set(s1))
-
-    if len1Unique < len1:
-        print('false')
-        return
-    print('true')
 
 if __name__ == "__main__":
     main()
